eecc8910cefe0e16635a703a6e379aa1eb083127
scgabriell <62763530+scgabriell@users.noreply.github.com>
2020-04-07 20:13:55 -0300

    Create Pais.java

---
+++ src/model/Pais.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Pais {
+
+	private int id;
+	private String nome;
+	private long populacao;
+	private double area;
+
+	public Pais(int id) {
+		this.id = id;
+	}
+
+	public Pais(int id, String nome, long populacao, double area) {
+		this.id = id;
+		this.nome = nome;
+		this.populacao = populacao;
+		this.area = area;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	@Override
+	public String toString() {
+		return "Pais [id=" + id + ", nome=" + nome + ", populacao= " + populacao + ", area=" + area + "]";
+	}
+
+}
